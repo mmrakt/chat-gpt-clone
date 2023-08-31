f969da9788f8890292327e68724b61ae43c4853d
mimu <akito.mimura@nijibox.co.jp>
2023-08-31 15:48:27 +0900

    add: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Star from "./star";
+import Lock from "./Lock";
 
 export default function Home() {
-  return <main className="text-red-500">main page</main>;
+  return (
+    <>
+      <header className="py-6 px-2 flex justify-center">
+        <div className=""></div>
+        <div className="flex bg-gray-200 rounded-lg p-1">
+          <button className="text-gray-700 flex justify-center items-center gap-2 py-2.5 bg-gray-500 w-36 rounded-lg ">
+            <Image src="/spark.svg" alt="" width={16} height={16} />
+            <span className="text-white text-sm">GPT-3.5</span>
+          </button>
+          <button className="text-gray-700 flex items-center gap-2 py-2.5  w-36 rounded justify-center">
+            {/* <Image src="/star.svg" alt="" width={16} height={16} /> */}
+            <Star />
+            <span className="text-sm">GPT-4</span>
+            <Lock />
+            {/* <Image src="/lock.svg" alt="" width={16} height={16} /> */}
+          </button>
+        </div>
+      </header>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
- 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          200: "#202123",
+          400: "#353441",
+          500: "#41404F",
+          600: "#555869",
+          700: "#8F8EA1",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
