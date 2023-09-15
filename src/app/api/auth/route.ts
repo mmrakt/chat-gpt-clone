bbf75d9aad3ac3d5986947bf8317361b85f24da4
mmrakt <mimuraakkun3@gmail.com>
2023-09-15 21:04:29 +0900

    fix basic auth api

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("WWW-authenticate", 'Basic realm="Secure Area"');
-  res.statusCode = 401;
-  res.end(`Auth Required.`);
-}
+export const GET = () => {
+  return new Response("Auth Required.", {
+    status: 401,
+    headers: { "WWW-authenticate": 'Basic realm="Secure Area"' },
+  });
+};
