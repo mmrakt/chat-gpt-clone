f4875e3bacfe6ab42c291de12ad0b63ea4806879
mimu <akito.mimura@nijibox.co.jp>
2023-09-20 10:45:30 +0900

    debugging

--- src/app/(routes)/c/[chatId]/page.tsx
+++ src/app/(routes)/c/[chatId]/page.tsx
@@ -4,6 +4,7 @@ import { fetchApi } from "@app/_utils";
 import { authOptions } from "@app/api/auth/[...nextauth]/route";
 import { Chat } from "@prisma/client";
 import { getServerSession } from "next-auth";
+import { prisma } from "@app/_libs/prisma";
 
 
 export default async function Page({ params }: { params: { chatId: string } }) {
@@ -11,11 +12,18 @@ export default async function Page({ params }: { params: { chatId: string } }) {
   if (!session) return;
 
   // const res = await fetchApi(`/chats/${params.chatId}`);
-  console.log(`${process.env.NEXT_PUBLIC_API_BASE_URL}/chats/${params.chatId}`);
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/chats/${params.chatId}`)
-  console.log('hoge');
-  console.log(res);
-  console.log(res.status);
+  // console.log(`${process.env.NEXT_PUBLIC_API_BASE_URL}/chats/${params.chatId}`);
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/chats/${params.chatId}`)
+  // console.log('hoge');
+  // console.log(res);
+  // console.log(res.status);
+  const chat = await prisma.chat.findUnique({
+    where: {
+      id: params.chatId,
+    },
+  });
+  console.log('foo');
+  console.log(chat);
   // if (res.status === 404) {
   //   notFound();
   // }
